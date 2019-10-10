9de64a8188eec46dfab4a8ff5985c851cc7fd19d
lzh <15138238380@163.com>
2019-10-10 15:44:01 +0800

    修改了一些配置文件，可直接配置到服务器上

---
+++ src/main/java/com/lzh0108/community/CommunityServletInitializer.java
@@ -0,0 +1,17 @@
+package com.lzh0108.community;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Tomcat会访问这个方法，通过这个方法作为入口来启动运行这个项目
+ */
+
+public class CommunityServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 这个项目的核心配置文件
+        return builder.sources(CommunityApplication.class);
+    }
+}
